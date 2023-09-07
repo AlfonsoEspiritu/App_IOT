dd05fa2f601cc08c715fd70e0b590af70e87fde5
AlfonsoEspiritu <alf-tron_007@hotmail.com>
2023-09-07 11:58:47 -0600

    Test translation

--- Unidad 2/server3.py
+++ Unidad 2/server3.py
@@ -1,71 +1,68 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from dotenv import load_dotenv
+import requests
 import json
+import os
 
-contador = 11
-
+text = ''
 
 class MyHTTPRequestHandler(BaseHTTPRequestHandler):
 
-  def _set_response(self, content_type="text/plain"):
-    self.send_response(200)
-    self.send_header("Content-type", content_type)
-    self.end_headers()
+    def _set_response(self, content_type="text/plain"):
+        self.send_response(200)
+        self.send_header("Content-type", content_type)
+        self.end_headers()
 
-  def throw_custom_error(self, message):
-    self._set_response("application/json")
-    self.wfile.write(json.dumps({"message": message}).encode())
+    def throw_custom_error(self, message):
+        self._set_response("application/json")
+        self.wfile.write(json.dumps({"message": message}).encode())
 
-  def do_GET(self):
-    self._set_response()
-    respuesta = "El valor es: " + str(contador)
-    self.wfile.write(respuesta.encode())
+    def do_GET(self):
+        self._set_response()
+        respuesta = "El valor es: " + str(text)
+        self.wfile.write(respuesta.encode())
 
-  def do_POST(self):
-    content_length = int(self.headers["Content-Length"])
-    post_data = self.rfile.read(content_length)
+    def do_POST(self):
+        content_length = int(self.headers["Content-Length"])
+        post_data = self.rfile.read(content_length)
 
-    try:
-      body_json = json.loads(post_data.decode())
-    except:
-      self.throw_custom_error("Invalid JSON")
-      return
+        try:
+            body_json = json.loads(post_data.decode())
+        except:
+            self.throw_custom_error("Invalid JSON")
+            return
 
-    global contador
+        # Check if action and plus are present
+        if ('text' not in body_json):
+            self.throw_custom_error("Missing text")
+            return
 
-    # Check if action and plus are present
-    if (body_json.get('action') is None or body_json.get('plus') is None):
-      self.throw_custom_error("Missing action or plus")
-      return
+        # Extract 'text' field from JSON
+        text = body_json.get('text', '')
+        print(text)
 
-    # Check if action is valid
-    if (body_json['action'] != 'asc' and body_json['action'] != 'desc'):
-      self.throw_custom_error("Invalid action")
-      return
+        load_dotenv()
+        X_RAPIDAPI_KEY = os.getenv("X_RAPIDAPI_KEY")
 
-    # Check if plus is valid, integer
-    try:
-      int(body_json['plus'])
-    except:
-      self.throw_custom_error("Invalid plus")
-      return
+        url = "https://google-translate1.p.rapidapi.com/language/translate/v2"
+        payload = {"q": text, "target": "en", "source": "es"}
+        headers = {
+            "content-type": "application/x-www-form-urlencoded",
+            "Accept-Encoding": "application/gzip",
+            "X-RapidAPI-Key": X_RAPIDAPI_KEY,
+            "X-RapidAPI-Host": "google-translate1.p.rapidapi.com"
+        }
 
-    if (body_json['action'] == 'asc'):
-      contador += int(body_json['plus'])
-    elif (body_json['action'] == 'desc'):
-      contador -= int(body_json['plus'])
-        
+        response = requests.post(url, data=payload, headers=headers)
+        texto_traducido = response.json()["data"]["translations"][0]["translatedText"]
 
-        # Print the complete HTTP request
-        #print("\n----- Incoming POST Request -----")
-        #print(f"Requestline: {self.requestline}")
-        #print(f"Headers:\n{self.headers}")
-        #print(f"Body:\n{post_data.decode()}")
-        #print("-------------------------------")
+        print(response.json())
+        print(texto_traducido)
 
         # Respond to the client
-    response_data = json.dumps({"message": "Received POST data, new data" + str(contador), "status": "Ok"})
-    self._set_response("application/json")
-    self.wfile.write(response_data.encode())
+        response_data = json.dumps({"message": "Received POST data, new data: " + texto_traducido, "status": "Ok"})
+        self._set_response("application/json")
+        self.wfile.write(response_data.encode())
 
 def run_server(server_class=HTTPServer, handler_class=MyHTTPRequestHandler, port=7800):
     server_address = ("", port)
